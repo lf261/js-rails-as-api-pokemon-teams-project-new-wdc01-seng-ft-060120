413ff7a4227e1deeb939f675e745a0a68b760303
steve <stevepesce879@gmail.com>
2020-07-20 17:52:52 -0400

    finished most frontend, just need to be able to release a ppokemon

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,13 +1,14 @@
 class PokemonsController < ApplicationController
-  
-  # def index
-  #   pokemons = Pokemon.all
-  #   render json: pokemons
-  # end
-  
+    
   # def show
   #   pokemon = Pokemon.find_by(params[:id])
   #   render json: pokemon
   # end
+  def create
+    name = Faker::Name.first_name
+    species = Faker::Games::Pokemon.name
+    poke = Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
+    render json: poke, :except => [:updated_at,:created_at]
+  end
 
 end
