a5062a6b95970e3043636e438c63e178ed6fbd09
steve <stevepesce879@gmail.com>
2020-07-20 15:28:13 -0400

    finished most backend, need add/remove functionality

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,7 @@
+class TrainersController < ApplicationController
+  def index
+    trainers = Trainer.all
+    render json: trainers, :include => {:pokemons => {:except => [:updated_at,:created_at]}},
+    :except => [:updated_at,:created_at]
+  end
+end
